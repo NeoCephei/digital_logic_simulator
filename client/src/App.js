e362eb2a57a9e9faac3c65520ecce21272517033
Magí Campo <magi.upc@gmail.com>
2021-11-10 16:28:21 +0100

    feat: form input, state management and style

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
-
+import GlobalContext from './services/globalContext'
 // Import Components here
 import Navbar from './components/navbar/navbar'
 import Dashboard from './components/dashboard/dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Dashboard />
-    </div>
+    <GlobalContext.Provider value = {'Hello from GC'}>
+      <div className="App">
+        <Navbar />
+        <Dashboard />
+      </div>
+    </GlobalContext.Provider>
   );
 }
 
--- client/src/components/dashboard/lc_form/lc_form.js
+++ client/src/components/dashboard/lc_form/lc_form.js
@@ -1,5 +1,5 @@
 import './lc_form.css';
-import React from 'react'
+import React, {useState} from 'react'
 
 //Import components here: (LC means LogicComponent)
 
@@ -7,12 +7,36 @@ import React from 'react'
 LC form ---> This one have a form with text input and create submit
 */
 
+
 function LcForm (props) {
+  
+  const [componentName, setComponentName] = useState('');
+  
+  function handleNameChange (e) {
+    setComponentName(e.target.value)
+  }
+  
+  function handleSubmit (e) {
+    e.preventDefault();
+    if (componentName.length < 1) {
+      alert('Please write a name');
+    } else {
+      console.log('From LC_Form: This is the name', componentName);
+      setComponentName('')
+    }
+  }
 
   return (
-    <div className="lc_form">
-      <h1>Hello from FORM</h1>
-    </div>
+    <form onSubmit={handleSubmit} className="lc_form">
+      <input 
+        type="text"
+        placeholder="Name your component"
+        value={componentName}
+        onChange ={handleNameChange}
+        className="form_componentName"
+      ></input>
+      <button type="submit">Create</button>
+    </form>
   )
 }
 
