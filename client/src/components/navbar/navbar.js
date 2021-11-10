856ea3fe18020ffb534a7032bf702e736fa91e2e
Magí Campo <magi.upc@gmail.com>
2021-11-10 15:03:04 +0100

    feat: client navbar

---
+++ client/src/components/navbar/navbar.js
@@ -0,0 +1,13 @@
+import './navbar.css'
+import React from 'react'
+
+function Navbar (props) {
+
+  return (
+    <div className="navbar">
+      <h1>Digital Logic Simulator</h1>
+    </div>
+  )
+}
+
+export default Navbar;
