0b4ac0b7bc9afe14ed1f9a23a6bcb8fa275de1de
Magí Campo <magi.upc@gmail.com>
2021-11-14 16:02:01 +0100

    tutorial: checking how to use canvas + react

--- client/src/components/canvas/canvas.js
+++ client/src/components/canvas/canvas.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useEffect, useRef}from 'react'
+
+import { BallMovement } from './ballMovement'
+import WallCollision from './wallCollision'
+import data from '../../data'
+
+
+let x = 0;
 
 export default function Canvas() {
-  return (
-    <div>
-      <h1>Canvas</h1>
-    </div>
-  )
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const render = () => {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d');
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      
+      let { ballObj} = data;
+
+      BallMovement(ctx, ballObj)
+      WallCollision (ballObj, canvas)
+
+      requestAnimationFrame(render);
+    }
+    render();
+  },[])
+
+  return <canvas id="canvas" ref={canvasRef} height="400" width="600"/>
 }
---
+++ client/src/data.js
@@ -0,0 +1,12 @@
+const data = {
+  ballObj : {
+    x:20,
+    y:200,
+    dx: 5,
+    dy: 5,
+    rad: 20,
+    speed: 5,
+  },
+}
+
+export default data;
